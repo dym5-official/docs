981697a07e366664008180bfa66edd0af8be2ef3
Dipak Acharya <dipak.acharya@webo.digital>
2023-09-01 09:42:22 +0545

    Create js object of menu

--- core/build.js
+++ core/build.js
@@ -7,6 +7,8 @@ const root = path.join(__dirname, '..');
 const distDir = path.join(root, 'dist');
 const template = fs.readFileSync(path.join(root, 'public', 'template.html')).toString();
 
+let theMenu = '';
+
 const ensureDir = (dir) => {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
@@ -16,11 +18,30 @@ const ensureDir = (dir) => {
 const writeFile = (file, content) => {
     ensureDir(path.dirname(file));
 
-    let html;
+    let html = template;
     
-    html = template.replace('{{--body--}}', content);
+    html = html.replace('{{--body--}}', content);
+    html = html.replace('{{--menu--}}', theMenu);
 
     fs.writeFileSync(file, html);
+}
+
+
+const createMenu = async (projectsDir, list) => {
+    const fullList = [];
+
+    for (const project of list) {
+        for (const doc of project) {
+            const { path: docPath, config: { menu } } = doc;
+
+            for (const sect in menu) {
+                const section = menu[sect];
+                fullList.push(section);
+            }
+        }
+    }
+
+    theMenu = JSON.stringify(fullList, null, 2);
 }
 
 const convertFiles = async (projectsDir, list) => {
@@ -59,6 +80,7 @@ const copyPublicFiles = async () => {
     const projectsDir = path.join(__dirname, '..', 'projects');
     const list = createFilesMap(projectsDir);
 
+    await createMenu(projectsDir, list);
     await convertFiles(projectsDir, list);
     await copyPublicFiles();
 })();
